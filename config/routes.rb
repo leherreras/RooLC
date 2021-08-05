203248784261a86d64c614bde92664b2c26ebaca
Luis Carlos Enrique Herrera Santos <leherreras@users.noreply.github.com>
2021-08-05 10:47:10 -0500

    Update config/routes.rb
    Co-authored-by: enzofab91 <enzofab91@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'registrations/sign_up_params'
-  mount_devise_token_auth_for 'User', at: 'api/v1'
+  mount_devise_token_auth_for 'User', at: 'api/v1', controllers: {
+    registrations: 'api/v1/registrations',
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
