c298c85b08da097f707c23dd59872f717e485ceb
Luis Carlos Enrique Herrera Santos <leherreras@users.noreply.github.com>
2021-08-10 13:39:32 -0500

    Update app/controllers/api/v1/passwords_controller.rb
    split the valitation
    
    Co-authored-by: Micaela Wolman <33032571+mwolman@users.noreply.github.com>

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -5,7 +5,7 @@ module Api
         # if a user is not found, return nil
         @resource = resource_class.with_reset_password_token(resource_params[:reset_password_token])
 
-        if @resource && @resource.reset_password_period_valid?
+        if @resource?.reset_password_period_valid?
           token = @resource.create_token unless require_client_password_reset_token?
 
           # ensure that user is confirmed
@@ -38,4 +38,3 @@ module Api
     end
   end
 end
-
