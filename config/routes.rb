b76ea9544ede4d135b84b61e8a8a8c1c9f9e71ca
Luis Herrera <leherreras@gmail.com>
2021-08-26 09:36:09 -0500

    delete the confirmations_controller.rb 'cause this functionality is of other branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/v1', controllers: {
-    registrations: 'api/v1/registrations',
-    confirmations: 'api/v1/confirmations'
+    registrations: 'api/v1/registrations'
   }
 end
