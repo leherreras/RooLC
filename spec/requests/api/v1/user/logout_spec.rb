b01b2c29af26d8431bc9683ea5ce2c9148a535d4
Luis Herrera <leherreras@gmail.com>
2021-08-30 09:31:18 -0500

    rubocop

--- spec/requests/api/v1/user/logout_spec.rb
+++ spec/requests/api/v1/user/logout_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "DELETE api/v1/users/sign_out", type: :request do
+RSpec.describe 'DELETE api/v1/users/sign_out', type: :request do
   describe 'DELETE session' do
-
     let(:user) { create(:user) }
 
-    it "logout a user" do
-
+    it 'logout a user' do
       # initial sign in to generate a token and response
       post user_session_path, params: {
         email: user.email,
@@ -17,12 +15,12 @@ RSpec.describe "DELETE api/v1/users/sign_out", type: :request do
 
       # sign out request using header values from sign in response
       delete destroy_user_session_path, params: {
-        "access-token": response.header["access-token"],
-        client: response.header["client"],
-        uid: response.header["uid"]
+        'access-token': response.header['access-token'],
+        client: response.header['client'],
+        uid: response.header['uid']
       }, as: :json
 
-      expect(json["errors"]).to be_blank
+      expect(json['errors']).to be_blank
       expect(response).to be_ok
       # user token should be deleted following sign out
       expect(user.reload.tokens.count).to eq 0
