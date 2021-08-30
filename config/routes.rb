f1cb8cddbfeab8a634c9d359ee447cf81b16bc97
Luis Herrera <leherreras@gmail.com>
2021-08-30 15:41:05 -0500

    create reset password

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/v1', controllers: {
     registrations: 'api/v1/registrations',
-    confirmations: 'api/v1/confirmations'
+    confirmations: 'api/v1/confirmations',
+    passwords: 'api/v1/passwords'
   }
 end
