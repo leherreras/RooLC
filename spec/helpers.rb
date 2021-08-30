024c134dc51b1e10fa14672678bc4aa1c94a36b1
Luis Herrera <leherreras@gmail.com>
2021-08-30 11:15:20 -0500

    rubocop

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -7,4 +7,8 @@ module Helpers
   def json
     JSON.parse(response.body).with_indifferent_access
   end
+
+  def auth_headers
+    @auth_headers ||= user.create_new_auth_token
+  end
 end
--- spec/requests/api/v1/user/logout_spec.rb
+++ spec/requests/api/v1/user/logout_spec.rb
@@ -1,29 +1,41 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'DELETE api/v1/users/sign_out', type: :request do
   describe 'DELETE session' do
     let(:user) { create(:user) }
 
-    it 'logout a user' do
-      # initial sign in to generate a token and response
-      post user_session_path, params: {
-        email: user.email,
-        password: user.password
-      }, as: :json
+    subject { delete destroy_user_session_path, headers: auth_headers, as: :json }
+    context 'when user is not logged in' do
+      it 'returns a successful response' do
+        subject
 
-      expect(user.reload.tokens.count).to eq 1
+        expect(response).to be_successful
+      end
 
-      # sign out request using header values from sign in response
-      delete destroy_user_session_path, params: {
-        'access-token': response.header['access-token'],
-        client: response.header['client'],
-        uid: response.header['uid']
-      }, as: :json
+      it 'logout a user' do
+        subject
 
-      expect(json['errors']).to be_blank
-      expect(response).to be_ok
-      # user token should be deleted following sign out
-      expect(user.reload.tokens.count).to eq 0
+        expect(json['errors']).to be_blank
+        expect(response).to be_ok
+        # user token should be deleted following sign out
+        expect(user.reload.tokens.count).to eq 0
+      end
+    end
+
+    context 'when user is not logged in' do
+      let(:auth_headers) { nil }
+
+      it 'does not return a successful response' do
+        subject
+        expect(response).to be_not_found
+      end
+
+      it 'does have empty session keys' do
+        subject
+        expect(user.reload.tokens).to be_empty
+      end
     end
   end
 end
