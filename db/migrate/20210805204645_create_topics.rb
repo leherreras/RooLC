9f5d4b8fbc494f2e7a1e7aa2e5510120dab14537
Luis Herrera <leherreras@gmail.com>
2021-08-31 16:53:04 -0500

    create the topics

---
+++ db/migrate/20210805204645_create_topics.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateTopics < ActiveRecord::Migration[6.1]
+  def change
+    create_table :topics do |t|
+      t.string :name, null: false
+      t.string :image
+
+      t.timestamps
+    end
+    add_index :topics, :name, unique: true
+  end
+end
---
+++ spec/factories/topics.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: topics
+#
+#  id         :bigint           not null, primary key
+#  image      :string
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_topics_on_name  (name) UNIQUE
+#
+FactoryBot.define do
+  factory :topic do
+    name { Faker::Games::WorldOfWarcraft.unique.hero }
+  end
+end
