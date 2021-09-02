0cd40f7e1492b73ab7ea9d938042807027e1f9c4
Luis Herrera <leherreras@gmail.com>
2021-09-01 21:14:49 -0500

    show the targets per client

--- app/views/api/v1/targets/_target.json.jbuilder
+++ app/views/api/v1/targets/_target.json.jbuilder
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 json.extract! target, :id, :title, :radius, :latitude, :longitude, :topic_id, :user_id, :created_at, :updated_at
-json.url api_v1_target_url(target, format: :json)
+json.url api_v1_targets_url(target, format: :json)
--- spec/routing/api/v1/targets_routing_spec.rb
+++ spec/routing/api/v1/targets_routing_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe Api::V1::TargetsController, type: :routing do
     it 'routes to #create' do
       expect(post: '/api/v1/targets').to route_to('api/v1/targets#create', format: 'json')
     end
+
+    it 'routes to #index' do
+      expect(get: '/api/v1/targets').to route_to('api/v1/targets#index', format: 'json')
+    end
   end
 end
