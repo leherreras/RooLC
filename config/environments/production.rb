6a3a3d0cfa81cd4760a41956d7b8da7a53de0755
Luis Herrera <leherreras@gmail.com>
2021-09-02 23:28:18 -0500

    add devise host in env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
+  config.action_mailer.default_url_options = { host: ENV['DEVISE_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "outlook.office365.com",
